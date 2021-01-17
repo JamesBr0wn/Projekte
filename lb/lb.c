23c569fbd9e75dd9bf3f765e051c3df0929f76b7
JamesBr0wn <j_brown_james@web.de>
2021-01-17 04:09:41 +0100

    led and button work

--- lb/lb.c
+++ lb/lb.c
@@ -4,42 +4,55 @@
 
 #include <pigpio.h>
 
+unsigned int led1Pin = 17;   // 4 is ok too
+unsigned int button1Pin = 22;
+
+void doExit()
+{
+	gpioWrite(led1Pin, 0); // don't keep it on... if it was on
+	gpioTerminate();
+}
+
 int main(int argc, char *argv[])
 {
+	// -------------------------- init Lib
 	printf("Start...\n");
-	unsigned int uhwrev;
-	uhwrev = gpioHardwareRevision();
+	unsigned int uhwrev = gpioHardwareRevision();
 	printf("gpioHardwareRevision %u unsigned, hx %x\n", uhwrev, uhwrev);
-
-  unsigned int gpioVer;
-  gpioVer = gpioVersion();
-	printf("gpioVersion = %u\n", gpioVer);
-
 
 	/* my PI returns 0xc03130 as revision from cat /proc/cpuinfo
 	unsigned int ui = 0xc03130;
-	printf("tst %u unsigned\n", ui);
 	*/
+  unsigned int gpioVer = gpioVersion();
+	printf("gpioVersion = %u\n", gpioVer);
 	
-	int status = gpioInitialise();
-	printf("initialise says: %d\n", status);
-	if (status < 0)
+	int libStatus = gpioInitialise();
+	printf("initialise says: %d\n", libStatus);
+	if (libStatus < 0)
 	{
 		exit(1);
 	}
 
-	// --------------------------
-	int ledPin1 = 4;
-	// int ledPin2 = 17;
-	int initLed1 = gpioSetMode(ledPin1, PI_OUTPUT);
-	printf("set output1 to GPIO %d: status: %d (0 means ok)\n", ledPin1, initLed1);
-	//int initLed2 = gpioSetMode(ledPin2, PI_OUTPUT);
-	//printf("set output2 to GPIO %d: status: %d (0 means ok)\n", ledPin2, initLed2);
-  int writeLed1 = gpioWrite(ledPin1, 1);
-  printf("write 1 GPIO %d: status: %d (0 means ok)\n", ledPin1, writeLed1);
+	// -------------------------- init Pins
+	int initLed1 = gpioSetMode(led1Pin, PI_OUTPUT);
+	printf("set output1 to GPIO %d: status: %d (0 means ok)\n", led1Pin, initLed1);
+	if (initLed1 < 0) doExit();
+	int initButton1 = gpioSetMode(button1Pin, PI_INPUT);
+	printf("set input1 to GPIO %d: status: %d (0 means ok)\n", button1Pin, initButton1);
+	if (initLed1 < 0) doExit();
+
+	// -------------------------- work
+  int writeLed1 = gpioWrite(led1Pin, 1);
+  printf("write 1 GPIO %d: status: %d (0 means ok)\n", led1Pin, writeLed1);
   
-	sleep(1);
-	// --------------------------
-	gpioWrite(ledPin1, 0); // don't keep it on
-	gpioTerminate();
+  int button1Status;
+  for (int i= 0; i< 100; i++)
+  {
+		button1Status = gpioRead(button1Pin);
+		printf("step %d get input1 status: %d (>=0 means ok)\n", i, button1Status);
+		usleep(200000); // in microseconds
+	}
+
+	// -------------------------- cleanup
+	doExit();
 }
