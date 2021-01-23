e4d5654276c926d084a5078d398852a936325dae
JamesBr0wn <j_brown_james@web.de>
2021-01-24 00:06:10 +0100

    smooth servo

--- lbs/lbs.c
+++ lbs/lbs.c
@@ -8,10 +8,10 @@ unsigned int led1Pin = 17;   // 4 is ok too
 unsigned int button1Pin = 22;
 unsigned int servo1Pin = 23;
 
-int doInit()
+int doInitLib()
 {
-	unsigned int uhwrev = gpioHardwareRevision();
-	printf("gpioHardwareRevision %u unsigned, hx %x\n", uhwrev, uhwrev);
+  unsigned int uhwrev = gpioHardwareRevision();
+  printf("gpioHardwareRevision %u unsigned, hx %x\n", uhwrev, uhwrev);
 
 	/* my PI returns 0xc03130 as revision from cat /proc/cpuinfo
 	unsigned int ui = 0xc03130;
@@ -19,13 +19,17 @@ int doInit()
 	unsigned int gpioVer = gpioVersion();
 	printf("gpioVersion = %u\n", gpioVer);
 
-	int libStatus = gpioInitialise();
+  int libStatus = gpioInitialise();
 	printf("initialise says: %d\n", libStatus);
 	if (libStatus < 0)
 	{
 		return -1;
 	}
+  return 1;
+}
 
+int doInit()
+{
 	// -------------------------- init Pins
 	int initLed1 = gpioSetMode(led1Pin, PI_OUTPUT);
 	printf("set output1 to GPIO %d: status: %d (0 means ok)\n", led1Pin, initLed1);
@@ -51,8 +55,13 @@ void button1AlertFunc(int gpio, int level, uint32_t tick)
 
 int main(int argc, char *argv[])
 {
+ 
 	// -------------------------- init Lib
 	printf("Start...\n");
+  if (doInitLib() < 0)
+  {
+		exit(1);		
+	}
   if (doInit() < 0)
   {
 		doExit();
@@ -78,6 +87,37 @@ int main(int argc, char *argv[])
   printf("input1 set alert: %d (>=0 means ok)\n", button1AlertSet);
   
   int servo1Status;
+  unsigned int pos = 1500;
+  // start in the middle
+  servo1Status = gpioServo(servo1Pin, pos);
+  printf("servo1 set to middle: %d (>=0 means ok)\n", servo1Status);
+  if (servo1Status < 0)
+  {
+		doExit();
+	}
+  sleep(2);
+
+  const int deltastep = 15;
+  int delta = deltastep;
+  const int servo1limitlow = 750;
+  const int servo1limithigh = 2350;
+  for (int i=0; i<1000; i++)
+  {
+		pos += delta;
+    servo1Status = gpioServo(servo1Pin, pos);
+    printf("servo1 pos set, status = %d (>=0 means ok), pos = %u\n", servo1Status, pos);
+    usleep(30000);
+    if (pos >= servo1limithigh)
+    {
+			delta = -1 * deltastep;
+		}
+		if (pos <= servo1limitlow)
+		{
+			delta = deltastep;
+		}
+	}
+
+	/*
   servo1Status = gpioServo(servo1Pin, 1500);
   printf("servo1 set status: %d (>=0 means ok)\n", servo1Status);
   sleep(5);
@@ -90,6 +130,7 @@ int main(int argc, char *argv[])
   servo1Status = gpioServo(servo1Pin, 1500);
   printf("servo1 set status: %d (>=0 means ok)\n", servo1Status);
   sleep(5);
+  */
 
 	// -------------------------- cleanup
 	doExit();
