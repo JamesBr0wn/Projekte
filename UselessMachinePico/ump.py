1c3db71aa18dc1a4947a8343942bed6ed4311a20
JamesBr0wn <j_brown_james@web.de>
2021-03-12 23:12:21 +0100

    deinit added

--- UselessMachinePico/ump.py
+++ UselessMachinePico/ump.py
@@ -1,17 +1,15 @@
 from machine import Pin, Timer, PWM
 import time
 
-led2 = Pin(25, Pin.OUT)  #internal LED
+led2 = Pin(25, Pin.OUT) #internal LED
 led  = Pin(14, Pin.OUT) #GP14
-btn  = Pin(28, Pin.IN)
+btn  = Pin(28, Pin.IN)  #GP28
 
-led2.value(1)
+led2.value(1) # switch on on program start
 
-
+# prepare servo
 servo = PWM(Pin(4))
 servo.freq(50)
-print("Servo = ")
-print(servo)
 
 pwmmin = 2200 # 3276
 pwmmax = 7900 # 6553
@@ -31,13 +29,11 @@ dc = pwmmin;
 count = 0
 while count < 100:
         if btn.value():
-            # print("Btn 1")
             led.value(1)
         else:
-            # print("Btn 0")
             led.value(0)
         servo.duty_u16(dc)
-        print("dc = " + str(dc))
+        # print("dc = " + str(dc))
         time.sleep(0.25)
         dc = dc + pwmdelta;
         if (dc > pwmmax):
@@ -50,10 +46,11 @@ while count < 100:
             print("umdreh min")
         count = count + 1;
 
-
+		#switch all off
 led.value(0)
 led2.value(0)
-servo.duty_u16(pwmmin)
+servo.deinit()
+
 
 
     
