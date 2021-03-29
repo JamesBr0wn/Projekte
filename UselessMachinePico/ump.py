7d499d2472cad8718f69f0e8fe8e61353c1f2818
JamesBr0wn <j_brown_james@web.de>
2021-03-29 04:09:20 +0200

    start status um

--- UselessMachinePico/ump.py
+++ UselessMachinePico/ump.py
@@ -1,41 +1,74 @@
 from machine import Pin, Timer, PWM
 import time
 
-# led2 = Pin(25, Pin.OUT) #internal LED
-led  = Pin(14, Pin.OUT) #GP14
-led2 = Pin(11, Pin.OUT)
+stateDict = {
+    "base":1,
+    "running":2
+    }
+
+# prepare Pins
+ledReady  = Pin(11, Pin.OUT)
+ledWork = Pin(14, Pin.OUT)
 btn  = Pin(28, Pin.IN)  #GP28
+servoArm = PWM(Pin(2))
+servoArm.freq(50)
+servoHood = PWM(Pin(4))
+servoHood.freq(50)
 
-led2.value(1) # switch on on program start
+class UMState:
+    startedTime = 0
+    
+    def __reset(self):
+        self.state = stateDict["base"]
+        self.startedTime = 0
+        
+    def __init__(self):
+        self.__reset()
 
-# prepare servo
-servo = PWM(Pin(4))
-servo.freq(50)
+    def getState(self):
+        return self.state
+
+    def startRunning(self):
+        if self.state != stateDict["base"]:
+            return -1
+        self.state = stateDict["running"]
+        self.startedTime = time.time()
+        return 1
+
+    def stopRunning(self):
+        self.__reset()
+
+    def doPrint(self):
+        print("State: ", self.state, " tick: ", self.startedTime)
+
+	def startApp(self):
+		ledReady.value(1) # switch on on program start
+		letWork.value(0)
+	
+	def stopApp(self):
+		ledReady.value(0)
+		ledWork.value(0)
+		servoHood.deinit()
+		servoArm.deinit()
+
 
 pwmmin = 2200 # 3276
 pwmmax = 7900 # 6553
 pwmstep = 200
 pwmdelta = pwmstep
-
 dc = pwmmin;
-# timer = Timer()
-# 
-# def blink(timer):
-#     led.toggle()
-#     led2.toggle()
-# 
-# timer.init(freq=1, mode=Timer.PERIODIC, callback=blink)
-#
+
+um = UMState()
+um.startApp()
 
 count = 0
 while count < 200:
         if btn.value():
-            led.value(1)
+            ledWork.value(1)
         else:
-            led.value(0)
-        servo.duty_u16(dc)
+            ledWork.value(0)
+        servoHood.duty_u16(dc)
         # print("dc = " + str(dc))
-        time.sleep(0.25)
         dc = dc + pwmdelta;
         if (dc > pwmmax):
             pwmdelta = -1 * pwmstep
@@ -46,11 +79,10 @@ while count < 200:
             dc = pwmmin
             print("umdreh min", dc)
         count = count + 1;
+        time.sleep(0.25)
 
-		#switch all off
-led.value(0)
-led2.value(0)
-servo.deinit()
+#switch all off
+um.stopApp()
 
 
 
